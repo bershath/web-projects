62cb2186c897b492dce88f98e0db6ca2a51e409f
bershath <bershath@gmail.com>
2024-09-06 23:18:33 +1000

    Initial commit

---
+++ servlet-request/src/main/java/org/bershath/labs/web/FirstServlet.java
@@ -0,0 +1,27 @@
+package org.bershath.labs.web;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        response.getWriter().append("<html><body>Received parameters: ").append(request.getContextPath()+firstName+" - "+lastName);
+        response.getWriter().append("<a href='/static-content/index.html'><br/>Go back</a></body></html>");
+
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
+}
